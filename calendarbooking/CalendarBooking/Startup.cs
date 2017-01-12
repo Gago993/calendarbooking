1c35102f49d9be9ee6a8e4e84c25aae8398171bc
Gago <g.ognjanovski@hotmail.com>
2017-01-12 15:00:42 +0100

    Initialize CalendarBooking

---
+++ calendarbooking/CalendarBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CalendarBooking.Startup))]
+namespace CalendarBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
