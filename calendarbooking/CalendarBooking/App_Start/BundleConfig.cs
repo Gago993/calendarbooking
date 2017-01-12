0813f7d3a8a2aac4ada8f519f8ca68a484f18487
viktorsamokov <viktor_veljanoski@hotmail.com>
2017-01-12 16:26:02 +0100

    Implement angular structure for front-end functionality

--- calendarbooking/CalendarBooking/App_Start/BundleConfig.cs
+++ calendarbooking/CalendarBooking/App_Start/BundleConfig.cs
@@ -14,6 +14,24 @@ namespace CalendarBooking
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular/library").Include(
+                        "~/Scripts/bower_components/angular/angular.js",
+                        "~/Scripts/bower_components/angular-route/angular-route.js",
+                        "~/Scripts/bower_components/angular-resource/angular-resource.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular/app").Include(
+                        "~/App/App.js",
+
+                        "~/App/DataServices/services.data.module.js",
+
+                        "~/App/Core/core.module.js",
+                        "~/App/Core/config.js",
+
+                        "~/App/Main/main.module.js",
+                        "~/App/Main/main.controller.js",
+
+                        "~/Scripts/bower_components/angular-resource/angular-resource.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
