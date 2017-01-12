195feaecf66ae34242caadbb80d7b6686ca9ca1e
viktorsamokov <viktor_veljanoski@hotmail.com>
2017-01-12 19:14:56 +0100

    Implement angular structure for frontend functionality

---
+++ calendarbooking/CalendarBookingProject/CalendarBookingProject/App/App.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict'
+
+    angular.module("app", [
+        "app.core",
+        "app.data",
+        "app.main",
+        "ui.calendar",
+        "ui.bootstrap"
+    ]);
+})();
