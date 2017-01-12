5b3581553d33fcd09002f6f932b9f895cb35f48e
Gago <g.ognjanovski@hotmail.com>
2017-01-12 17:19:53 +0100

    Add User Migration

---
+++ calendarbooking/CalendarBooking/DAL/CalendarBookingContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CalendarBooking.DAL
+{
+    public class CalendarBookingContext : DbContext
+    {
+        public CalendarBookingContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Booking> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
